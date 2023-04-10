ebc3faf9e83ca59919d316e11970a48da2e0cf81
nanggo <yamsiri@gmail.com>
2023-04-10 10:07:28 +0900

    Add vercel analytics

--- pages/_app.js
+++ pages/_app.js
@@ -12,6 +12,8 @@ import Analytics from '@/components/analytics'
 import LayoutWrapper from '@/components/LayoutWrapper'
 import { ClientReload } from '@/components/ClientReload'
 
+import { Analytics as VercelAnalytics } from '@vercel/analytics/react'
+
 const isDevelopment = process.env.NODE_ENV === 'development'
 const isSocket = process.env.SOCKET
 
@@ -25,6 +27,7 @@ export default function App({ Component, pageProps }) {
       <Analytics />
       <LayoutWrapper>
         <Component {...pageProps} />
+        <VercelAnalytics />
       </LayoutWrapper>
     </ThemeProvider>
   )
